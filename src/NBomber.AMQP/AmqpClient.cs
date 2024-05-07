0d6cfba82c1f5349df1cd38ade1d45e73524edd0
Andrii <Andrey1w2005@gmail.com>
2024-05-07 16:34:40 +0300

    tab length from 3 to 4

--- src/NBomber.AMQP/AmqpClient.cs
+++ src/NBomber.AMQP/AmqpClient.cs
@@ -8,96 +8,96 @@ namespace NBomber.AMQP;
 
 public class AmqpClient(IChannel channel)
 {
-   public IChannel Channel { get; } = channel;
+	public IChannel Channel { get; } = channel;
 
-   private readonly Channel<Response<BasicDeliverEventArgs>> _queue = 
-      System.Threading.Channels.Channel.CreateUnbounded<Response<BasicDeliverEventArgs>>();
+	private readonly Channel<Response<BasicDeliverEventArgs>> _queue =
+		System.Threading.Channels.Channel.CreateUnbounded<Response<BasicDeliverEventArgs>>();
 
-   public Response<object> Publish<TProperties>(string exchange, string routingKey, 
-      in TProperties basicProperties, ReadOnlyMemory<byte> body = default, bool mandatory = false)
-      where TProperties : IReadOnlyBasicProperties, IAmqpHeader
-   {
-       Channel.BasicPublishAsync(exchange, routingKey, basicProperties, body, mandatory);
+	public Response<object> Publish<TProperties>(string exchange, string routingKey,
+		in TProperties basicProperties, ReadOnlyMemory<byte> body = default, bool mandatory = false)
+		where TProperties : IReadOnlyBasicProperties, IAmqpHeader
+	{
+		Channel.BasicPublishAsync(exchange, routingKey, basicProperties, body, mandatory);
 
-       var sizeBytes = body.Length + exchange.Length + routingKey.Length +
-                       GetSizeBytesOfBasicProperties(basicProperties);
-      
-       return Response.Ok(sizeBytes: sizeBytes);
-   }
-   
-   public Response<object> Publish<TProperties>(CachedString exchange, CachedString routingKey, 
-      in TProperties basicProperties, ReadOnlyMemory<byte> body = default, bool mandatory = false)
-      where TProperties : IReadOnlyBasicProperties, IAmqpHeader
-   {
-      Channel.BasicPublishAsync(exchange, routingKey, basicProperties, body, mandatory);
+		var sizeBytes = body.Length + exchange.Length + routingKey.Length +
+		                GetSizeBytesOfBasicProperties(basicProperties);
 
-      var sizeBytes = body.Length + exchange.Bytes.Length + routingKey.Bytes.Length 
-                      + GetSizeBytesOfBasicProperties(basicProperties);
-      
-      return Response.Ok(sizeBytes: sizeBytes);
-   }
-   
-   public Response<object> Publish<T>(PublicationAddress addr, in T basicProperties,
-      ReadOnlyMemory<byte> body) where T : IReadOnlyBasicProperties, IAmqpHeader
-   {
-      Channel.BasicPublishAsync(addr, basicProperties, body);
+		return Response.Ok(sizeBytes: sizeBytes);
+	}
 
-      var sizeBytes = body.Length + addr.RoutingKey.Length + addr.ExchangeName.Length + addr.ExchangeType.Length 
-                      + GetSizeBytesOfBasicProperties(basicProperties);
-      
-      return Response.Ok(sizeBytes: sizeBytes);
-   }
-   
-   public void AddConsumer(string queue, bool autoAck)
-   {
-      var consumer = new EventingBasicConsumer(Channel);
-      consumer.Received += (model, ea) =>
-      {
-         var sizeBytes = GetSizeBytesOfBasicProperties(ea.BasicProperties);
+	public Response<object> Publish<TProperties>(CachedString exchange, CachedString routingKey,
+		in TProperties basicProperties, ReadOnlyMemory<byte> body = default, bool mandatory = false)
+		where TProperties : IReadOnlyBasicProperties, IAmqpHeader
+	{
+		Channel.BasicPublishAsync(exchange, routingKey, basicProperties, body, mandatory);
 
-         sizeBytes += ea.Body.Length;
-         sizeBytes += ea.ConsumerTag.Length;
-         sizeBytes += ea.RoutingKey.Length;
-         
-         _queue.Writer.WriteAsync(Response.Ok(payload: ea, sizeBytes: sizeBytes));
-      };
+		var sizeBytes = body.Length + exchange.Bytes.Length + routingKey.Bytes.Length
+		                + GetSizeBytesOfBasicProperties(basicProperties);
 
-       Channel.BasicConsume(queue: queue, autoAck: autoAck, consumer: consumer);
-   }
+		return Response.Ok(sizeBytes: sizeBytes);
+	}
 
-   private static long GetSizeBytesOfBasicProperties(IReadOnlyBasicProperties basicProperties)
-   {
-      var sizeBytes = 0;
-      
-      if (basicProperties.IsHeadersPresent())
-      {
-         sizeBytes = basicProperties.Headers!.Sum(kv =>
-         {
-            var result = kv.Key.Length;
-            result += kv.Value is byte[] bytes ? bytes.Length : 0;
-            result += kv.Value is string str ? str.Length : 0;
-               
-            return result;
-         });
-      }
-      
-      sizeBytes += basicProperties.Expiration?.Length ?? 0;
-      sizeBytes += basicProperties.ClusterId?.Length ?? 0;
-      sizeBytes += basicProperties.ContentEncoding?.Length ?? 0;
-      sizeBytes += basicProperties.CorrelationId?.Length ?? 0;
-      sizeBytes += basicProperties.ContentType?.Length ?? 0;
-      sizeBytes += basicProperties.Type?.Length ?? 0;
-      sizeBytes += basicProperties.AppId?.Length ?? 0;
-      sizeBytes += basicProperties.MessageId?.Length ?? 0;
-      sizeBytes += basicProperties.ReplyTo?.Length ?? 0;
-      sizeBytes += basicProperties.UserId?.Length ?? 0;
-      
-      sizeBytes += basicProperties.ReplyToAddress?.ExchangeName.Length ?? 0;
-      sizeBytes += basicProperties.ReplyToAddress?.RoutingKey.Length ?? 0;
-      sizeBytes += basicProperties.ReplyToAddress?.ExchangeType.Length ?? 0;
+	public Response<object> Publish<T>(PublicationAddress addr, in T basicProperties,
+		ReadOnlyMemory<byte> body) where T : IReadOnlyBasicProperties, IAmqpHeader
+	{
+		Channel.BasicPublishAsync(addr, basicProperties, body);
 
-      return sizeBytes;
-   }
+		var sizeBytes = body.Length + addr.RoutingKey.Length + addr.ExchangeName.Length + addr.ExchangeType.Length
+		                + GetSizeBytesOfBasicProperties(basicProperties);
 
-   public ValueTask<Response<BasicDeliverEventArgs>> Receive() => _queue.Reader.ReadAsync();
+		return Response.Ok(sizeBytes: sizeBytes);
+	}
+
+	public void AddConsumer(string queue, bool autoAck)
+	{
+		var consumer = new EventingBasicConsumer(Channel);
+		consumer.Received += (model, ea) =>
+		{
+			var sizeBytes = GetSizeBytesOfBasicProperties(ea.BasicProperties);
+
+			sizeBytes += ea.Body.Length;
+			sizeBytes += ea.ConsumerTag.Length;
+			sizeBytes += ea.RoutingKey.Length;
+
+			_queue.Writer.WriteAsync(Response.Ok(ea, sizeBytes: sizeBytes));
+		};
+
+		Channel.BasicConsume(queue, autoAck, consumer);
+	}
+
+	private static long GetSizeBytesOfBasicProperties(IReadOnlyBasicProperties basicProperties)
+	{
+		var sizeBytes = 0;
+
+		if (basicProperties.IsHeadersPresent())
+		{
+			sizeBytes = basicProperties.Headers!.Sum(kv =>
+			{
+				var result = kv.Key.Length;
+				result += kv.Value is byte[] bytes ? bytes.Length : 0;
+				result += kv.Value is string str ? str.Length : 0;
+
+				return result;
+			});
+		}
+
+		sizeBytes += basicProperties.Expiration?.Length ?? 0;
+		sizeBytes += basicProperties.ClusterId?.Length ?? 0;
+		sizeBytes += basicProperties.ContentEncoding?.Length ?? 0;
+		sizeBytes += basicProperties.CorrelationId?.Length ?? 0;
+		sizeBytes += basicProperties.ContentType?.Length ?? 0;
+		sizeBytes += basicProperties.Type?.Length ?? 0;
+		sizeBytes += basicProperties.AppId?.Length ?? 0;
+		sizeBytes += basicProperties.MessageId?.Length ?? 0;
+		sizeBytes += basicProperties.ReplyTo?.Length ?? 0;
+		sizeBytes += basicProperties.UserId?.Length ?? 0;
+
+		sizeBytes += basicProperties.ReplyToAddress?.ExchangeName.Length ?? 0;
+		sizeBytes += basicProperties.ReplyToAddress?.RoutingKey.Length ?? 0;
+		sizeBytes += basicProperties.ReplyToAddress?.ExchangeType.Length ?? 0;
+
+		return sizeBytes;
+	}
+
+	public ValueTask<Response<BasicDeliverEventArgs>> Receive() => _queue.Reader.ReadAsync();
 }
